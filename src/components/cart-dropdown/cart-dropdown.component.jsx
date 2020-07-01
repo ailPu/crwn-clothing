afeeb1336b92d0eb07bfa74bbb1c7472919cb223
Paul Baur <paulbaur@gmx.at>
2020-07-01 17:29:36 +0200

    updates shop data with new data, updates cart utils to group cart items in cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
 	<div className="cart-dropdown">
-		<div className="cart-items" />
+		<div className="cart-items">
+			{cartItems.map((item) => (
+				<div className="cart-item">
+					<div
+						key={item.id}
+						className="image"
+						style={{ backgroundImage: `url(${item.imageUrl})` }}
+					/>
+					<div>{item.name}</div>
+				</div>
+			))}
+		</div>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
