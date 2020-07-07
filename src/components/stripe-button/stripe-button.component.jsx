50399ff2aad2d31e2f216724eb7766fb72d4c749
Paul Baur <paulbaur@gmx.at>
2020-07-07 14:37:43 +0200

    updates stripe integration and updates favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+import { createLogger } from "redux-logger";
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	const publishableKey =
+		"pk_test_51H2FJILXoE0RkQdqqTr47PxxNU1bADu9wGeegOgLhWpxgSIAZOcejqHRStRZv4T4QmcI5JkCdT9a1IE4Gi3bzFlM009gpguurz";
+	//! token ist die Callback Funktion und würde dann zum backend fahren, was hier nicht passiert
+	const onToken = (token) => {
+		console.log(token);
+		alert("Payment Successful");
+	};
+
+	return (
+		<StripeCheckout
+			label="Pay Now"
+			name="CRWN Clothing Ltd."
+			billingAddress
+			shippingAddress
+			image="https://svgshare.com/i/CUz.svg"
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel="Pay Now"
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,7 +6,9 @@ import {
 	selectCartItems,
 	selectCartTotal,
 } from "../../redux/cart/cart.selectors";
+
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -35,6 +37,12 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
 		<div className="total">
 			<span>TOTAL: ${cartTotal}</span>
 		</div>
+		<div className="test-warning">
+			*Please use the following thest credit card for payment
+			<br />
+			4242 4242 4242 4242 - Exp: 08/20 - CVV: 123*
+		</div>
+		<StripeCheckoutButton price={cartTotal} />
 	</div>
 );
 
